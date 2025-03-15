8e7c0b1b11e1d620bc470d0db73df13d58724684
Cyber-Eagle404 <dimsumbeneran@gmail.com>
2025-03-15 17:43:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function ubahTeks() {
-    document.getElementById("judul").innerText = "Terima Kasih Sudah Klik!";
-    alert("Teks telah berubah!");
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            window.scrollTo({
+                top: target.offsetTop - 50,
+                behavior: "smooth"
+            });
+        });
+    });
+});
